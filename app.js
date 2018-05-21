d522201803e0cd8b5c8dbf96697f0d76bee5d219
caowei <solocao@126.com>
2018-05-21 16:16:59 +0800

    插入数据

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ const connection = new Sequelize('demo_schema', 'root', 'root', {
 
 const Article = connection.define('article', {
   title: Sequelize.STRING,
-  content: Sequelize.STRING
+  content: Sequelize.TEXT
 })
 
 // sync 做两件事情
 // 第一、连接数据库
 // 第二、创建一个对应数据库
-connection.sync({
-  logging: console.log
+connection.sync().then(() => {
+  Article.create({
+    title: 'demo title',
+    content: 'Lorem ipsum dolor'
+  })
 })
